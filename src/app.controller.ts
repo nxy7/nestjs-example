0bf92455205509f919943ce8cddf2711d06bc9ae
nxy7 <lolnoxy@gmail.com>
2024-03-13 21:35:59 +0100

    config service added
    Signed-off-by: nxy7 <lolnoxy@gmail.com>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService, private readonly config: ConfigService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/env")
+  getEnv(): string {
+   return  this.config.get("testenv") ?? "none"
   }
 }
